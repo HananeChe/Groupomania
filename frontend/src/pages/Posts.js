0a9c613e0155bc1baf93789dcce389934e906e11
HananeChe <104737255+HananeChe@users.noreply.github.com>
2022-10-13 15:43:20 +0200

    gestion Like/dislike et deletepost

--- frontend/src/pages/Posts.js
+++ frontend/src/pages/Posts.js
@@ -1,22 +1,34 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom' 
+import React, { useEffect, useState, useContext } from 'react'
+import { Link, useParams } from 'react-router-dom' 
 import Navbar from '../components/Navbar';
-import State from '../components/State';
+import LogContext from '../config/LogContext';
+import './Posts.css';
 
 //import { Loader } from '../components/Loader';
 
 
 export default function Posts() {
+
    //const [isLoading, setLoading] = useState(true);
    const [data, setData]= useState([]); 
 
+
+   //contexte
+  const logCtx = useContext(LogContext);
   
   const getAllPost = () => {
-    return fetch('http://localhost:3001/api/posts')
+    return fetch('http://localhost:3001/api/posts',{
+      method: "GET",
+      headers:{
+        "Content-type" : "application/json",
+        Authorization: `Bearer ${logCtx.token}`
+      }
+    
+    })
       .then((res) => res.json())
       .then((data) => {
         setData(data)
-        }, console.log(data))
+        })
       .catch((error) => {
         console.error(error);
       })
@@ -25,22 +37,27 @@ export default function Posts() {
     useEffect(() => {
       getAllPost();
     }, );
+    
 
-  
     return (
        <div>
         <div>
           <Navbar />
         </div>
-        <div>
-          <State />
-        </div>
-        <div>
+        <div className='containerPosts'>
           <h1>Les derniers posts:</h1>
             {data.map(dataPost => (
-            <div key={dataPost._id}>
-            <Link to={"/post/" + dataPost._id} key={"post" + dataPost._id}>{dataPost.userName}</Link>
+            <div key={dataPost._id} className="post">
+            <Link to={"/post/" + dataPost._id} key={"post" + dataPost._id} className="linkPost">{dataPost.userName} {dataPost.userId}:</Link>
             <p key={"content" + dataPost._id}>{dataPost.content}</p>
+            <div className='comment'>
+            <label htmlFor='comment'>Commentaires:</label>
+            <input type="text" name="comment"/>
+            </div>
+            <div className='btn'>
+              <button>Like</button>
+              <button>Dislike</button>
+            </div>
             </div>
             //<p key={dataPost._id}>{dataPost.userName} dit:{dataPost.content}</p>
             ))}
